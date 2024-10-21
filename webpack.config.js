9ff69062df4c200ef113c7e30a481912a3fe9e26
xintongj <114182195+xintongj@users.noreply.github.com>
2024-10-21 01:01:31 -0400

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'), // or wherever the assets folder is
+        },
+        compress: true,
+        port: 9000,
+    },
+};
